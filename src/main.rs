6694cf0c8e120b77c24541777aa0e4eea2b497f7
artslob <artslob@yandex.ru>
2020-11-29 18:14:26 +0300

    first ppm image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+    const BRIGHTNESS: i32 = 255;
+
+    // header of ppm image file
+    println!("P3\n{} {}\n{}", IMAGE_WIDTH, IMAGE_HEIGHT, BRIGHTNESS);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let red = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let green = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let blue: f64 = 0.25;
+
+            const ROUNDING: f64 = 255.0;
+
+            let red = (ROUNDING * red) as i32;
+            let green = (ROUNDING * green) as i32;
+            let blue = (ROUNDING * blue) as i32;
+
+            print!("{} {} {} ", red, green, blue);
+        }
+        println!();
+    }
 }
