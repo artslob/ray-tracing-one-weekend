33d3b7adcba417057cd58408261017da99d42793
artslob <artslob@yandex.ru>
2020-11-29 23:20:59 +0300

    add Vec3 struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    fn origin() -> Self {
+        Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+
+    fn new(x: f64, y: f64, z: f64) -> Self {
+        Vec3 { x, y, z }
+    }
+}
+
 fn main() {
     const IMAGE_WIDTH: i32 = 256;
     const IMAGE_HEIGHT: i32 = 256;
