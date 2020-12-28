1a7d75adcdda58ac3969a3b6967a50d667f78494
artslob <artslob@yandex.ru>
2020-12-28 21:28:27 +0300

    shading with surface normals

--- src/main.rs
+++ src/main.rs
@@ -192,8 +192,21 @@ impl Ray {
     }
 
     fn ray_color(&self) -> Color {
-        if self.hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5) {
-            return Color::new(1.0, 0.0, 0.0);
+        let t = self.hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5);
+        if let Some(t) = t {
+            let n = (self.at(t)
+                - Vec3 {
+                    x: 0.0,
+                    y: 0.0,
+                    z: -1.0,
+                })
+            .unit_vector();
+            return 0.5
+                * Color {
+                    x: n.x + 1.0,
+                    y: n.y + 1.0,
+                    z: n.z + 1.0,
+                };
         }
         let unit_direction = self.direction.unit_vector();
         let t = 0.5 * (unit_direction.y + 1.0);
@@ -203,13 +216,17 @@ impl Ray {
         (1.0 - t) * white + t * Color::new(0.5, 0.7, 1.0)
     }
 
-    fn hit_sphere(&self, center: Point3, radius: f64) -> bool {
+    fn hit_sphere(&self, center: Point3, radius: f64) -> Option<f64> {
         let oc: Vec3 = Vec3::origin() - center;
         let a = self.direction.dot(&self.direction);
         let b = 2.0 * oc.dot(&self.direction);
         let c = oc.dot(&oc) - radius.powi(2);
         let discriminant = b.powi(2) - 4.0 * a * c;
-        discriminant > 0.0
+        if discriminant < 0.0 {
+            None
+        } else {
+            Some((-b - discriminant.sqrt()) / (2.0 * a))
+        }
     }
 }
 
