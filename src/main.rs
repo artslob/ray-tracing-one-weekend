82d279dcfa50ae0968853344f3a25424ac1bc9e5
artslob <artslob@yandex.ru>
2020-12-30 21:37:52 +0300

    6.3: An Abstraction for Hittable Objects

--- src/main.rs
+++ src/main.rs
@@ -235,6 +235,59 @@ impl Ray {
     }
 }
 
+struct HitRecord {
+    point: Point3,
+    normal: Vec3,
+    t: f64,
+}
+
+trait Hittable {
+    fn hit(&self, ray: &Ray, min: f64, max: f64) -> Option<HitRecord>;
+}
+
+struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Sphere {
+    fn new(center: Point3, radius: f64) -> Self {
+        Self { center, radius }
+    }
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, ray: &Ray, min: f64, max: f64) -> Option<HitRecord> {
+        let oc = ray.origin - self.center;
+        let a = ray.direction.length_squared();
+        let half_b = oc.dot(&ray.direction);
+        let c = oc.length_squared() - self.radius.powi(2);
+
+        let discriminant = half_b.powi(2) - a * c;
+        if discriminant < 0.0 {
+            return None;
+        }
+
+        let sqrt_discriminant = discriminant.sqrt();
+
+        let mut root = (-half_b - sqrt_discriminant) / a;
+
+        if root < min || root > max {
+            root = (-half_b + sqrt_discriminant) / a;
+            if root < min || root > max {
+                return None;
+            }
+        }
+
+        let point = ray.at(root);
+        Some(HitRecord{
+            point,
+            normal: (point - self.center) / self.radius,
+            t: root
+        })
+    }
+}
+
 fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
 
