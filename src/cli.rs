966fe8b1cd043577f3a9537d05222bf5a67d4fc1
artslob <artslob@yandex.ru>
2025-08-09 14:55:42 +0300

    add cli args

--- src/cli.rs
+++ src/cli.rs
@@ -4,4 +4,8 @@ pub struct Args {
     /// No multithreading
     #[arg(short, long, default_value_t = false)]
     pub single_thread: bool,
+    #[arg(long, default_value_t = 500)]
+    pub samples_per_pixel: u32,
+    #[arg(long, default_value_t = 50)]
+    pub max_depth: u32,
 }
